dfbe62a7f2e416727a17d36e572b750324af4589
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-02-13 00:00:12 +0100

    subscraping
    Signed-off-by: Roy van Kaathoven <royvankaathoven@hotmail.com>

--- src/main/scala/org/rovak/scraper/ScrapeApp.scala
+++ src/main/scala/org/rovak/scraper/ScrapeApp.scala
@@ -3,16 +3,25 @@ package org.rovak.scraper
 import akka.actor._
 
 object ScrapeApp extends App {
-  
+
   val system = ActorSystem()
+  system.actors
+  
   val scraper = system.actorOf(Props[actors.WebsiteScraper])
+  
 
   println("Starting!")
-  
-  var links: List[String] = List("https://www.google.com/search?q=scala")
 
-  scraper ! actors.LinkList(links)
- 
+  var links: List[String] = List(
+      "https://www.google.com/search?q=scala",
+      "https://www.google.com/search?q=php",
+      "https://www.google.com/search?q=javascript"
+      )
+
+  for (link <- links) {
+    println("Scraping website: " + link)
+    scraper ! actors.Website(link)
+  }
+
   println("Scrape request send!")
-
 }
--- src/main/scala/org/rovak/scraper/actors/WebsiteScraper.scala
+++ src/main/scala/org/rovak/scraper/actors/WebsiteScraper.scala
@@ -5,54 +5,82 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import scala.collection.JavaConversions._
 
-case class LinkList(links: List[String])
+case class Links(links: List[String])
 case class LinkResult(results: List[String])
+case class Website(url: String)
 
 /**
  * Simple actor who retrieves websites and searches for links
  */
 class WebsiteScraper extends Actor {
-  
+
+  /**
+   * @param link String A link from a Google search result
+   * @return String
+   */
+  def retrieveGoogleLink(link: String) = {
+    link.substring(link.indexOf("?q=") + 3, link.indexOf("&sa"))
+  }
+
   /**
    * @param url String A website url which will be scraped for links
    * @return List[(String, String)]
    */
-  def getLinks(url: String) : List[(String, String)] = {
-    val doc = Jsoup.connect(url).userAgent("Mozilla").timeout(0).get()
-    val links = doc.select("a").map(x => (
-					x.select("a[href]").attr("abs:href"),
-					x.select("a[href]").text
-				)).toList
-    				
-    for (link <- links) {
-      println("Found href" + link)
+  def getLinks(url: String): List[(String, String)] = {
+
+    var links: List[(String, String)] = List()
+
+    try {
+      val doc = Jsoup.connect(url).userAgent("Mozilla").timeout(0).get()
+
+      if (url.indexOf("www.google.com") > 0) {
+        links = doc.select("#res li.g h3.r a").map(x => (
+          retrieveGoogleLink(x.select("a[href]").attr("abs:href")),
+          x.select("a[href]").text)).toList
+      } else {
+        links = doc.select("a").map(x => (
+          x.select("a[href]").attr("abs:href"),
+          x.select("a[href]").text)).toList
+      }
+
+      for (link <- links) {
+        println("Url: " + link._1 + ", Website: " + link._2)
+        val websiteScraper = context.system.actorOf(Props[WebsiteScraper])
+        websiteScraper ! Website(link._1)
+      }
+    } catch {
+      case e: Exception => { println("Could not scrape url: " + e.getMessage()) }
     }
+
     links
   }
-  
+
   /**
    * Iterate through a list of websites and call the getLinks for each
-   * 
+   *
    * @param links List[String]
    * @return List[String]
    */
   def scrapeLinks(links: List[String]): List[String] = {
-		  
+
     for (link <- links) {
-      println("Scraping website: " + link)
-      getLinks(link)
+      val websiteScraper = context.system.actorOf(Props[WebsiteScraper])
+      websiteScraper ! Website(link)
     }
-    
+
     // TODO: valid return value
     List("Return value")
   }
-  
+
   /**
    * Message Listener
    */
   def receive = {
-    case LinkList(links) => {
+    case Links(links) => {
       scrapeLinks(links)
+    }
+    case Website(url) => {
+      getLinks(url)
     }
   }
 }
